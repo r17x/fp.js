8f46444c9a056b53f20f779755e8647e7d6a8618
✶ DO NOT EDIT <fncolon@pm.me>
2022-02-04 02:14:29 +0700

    chore(chunk): code is documentation

--- src/chunk.js
+++ src/chunk.js
@@ -4,7 +4,7 @@ import { length } from "./length";
  * Turn an array into smaller arrays of the given size
  *
  * @param {Array} arr - An array or map of values.
- * @param {number} size - Size of the values inside a collection, if it exceeds the size, continue with a new collection
+ * @param {number} size - Size of the values inside an array
  * @return {Array} - returns an array of smaller arrays of the given size
  * @example
  * ```javascript
